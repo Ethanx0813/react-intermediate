55efc9e42b56556cdd8903c6f15199514d952e23
Ethanx0813 <ankityadav770587@gmail.com>
2024-01-14 11:33:31 +0530

    prop-drilling

---
+++ src/ChildComponent.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import GrandchildComponent from './GrandchildComponent';
+
+function ChildComponent(props) {
+  return (
+    <div>
+      <p>Data from Child: {props.data}</p>
+      <GrandchildComponent data={props.data} />
+    </div>
+  );
+}
+
+export default ChildComponent;
